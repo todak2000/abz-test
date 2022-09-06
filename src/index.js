ef53ba9fd798b206505f1711732ef6d34ef6c672
Daniel Olagunju <todak2000@gmail.com>
2022-09-06 14:07:03 +0100

    ptimizing for page speed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import './fonts/Goldman/Goldman-Bold.ttf';
+import '../src/Assets/Fonts/Nunito/Nunito-Regular.ttf'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
